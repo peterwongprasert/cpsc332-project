fd4392c38be5e8cb7ae9ac061ef2fabb1c989bf7
peterwongprasert <peterwongprasert@gmail.com>
2024-04-30 01:00:10 -0700

    more stuff

--- signin.php
+++ signin.php
@@ -2,4 +2,17 @@
 
 require 'connect.php';
 
+if(isset($_POST['username'])){
+  $name = htmlspecialchars($_POST['username']);
+}
+if(isset($_POST['password'])){
+  $pw = htmlspecialchars($_POST['password']);
+}  
+
+$sql = "SELECT COUNT(*) 
+  FROM `User` 
+  WHERE Name = ? AND Password = ?";
+
+
+
 ?>
